e87f07b163bd77d0d571e9c5deca76b203b9e13d
Bruno Jullien <bruno.jullien@gmail.com>
2022-01-26 07:25:20 -0800

    15. Type Assignment & Type Inference

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("your code goes here");
+// function add(n1: number, n2: number) {
+//     return n1 + n2;
+// }
+var add = function (n1, n2, showResult, phrase) {
+    var res = n1 + n2;
+    if (showResult) {
+        console.log(phrase + res);
+    }
+    else {
+        return n1 + n2;
+    }
+};
+var number1 = 5; //same type as 5.0
+var number2 = 2.8;
+var printResult = true;
+var resultPhrase = 'the result is ';
+add(number1, number2, printResult, resultPhrase);
